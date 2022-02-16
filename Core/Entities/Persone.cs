af557ed3c98cf55f6e7145a32769d37f90225b81
AnwarAek <aitelkosari_anwar@outlook.fr>
2022-02-16 12:58:23 +0100

    Add project files.

---
+++ Core/Entities/Persone.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Persone : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
